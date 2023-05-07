e91d8a41ac7a8f230c83dce9181534abf6b9729b
aixoio <79969530+aixoio@users.noreply.github.com>
2023-05-07 10:51:27 +1000

    Init

---
+++ src/main/java/github/aixoio/antimobs/AntiMobs.java
@@ -0,0 +1,17 @@
+package github.aixoio.antimobs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AntiMobs extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
